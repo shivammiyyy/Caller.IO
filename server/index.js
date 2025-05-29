f961a5c53162a01e8abc1b1064e44364e232fd98
Shivam Kumar <shivamprofessional1@gmail.com>
2025-05-30 03:45:30 +0530

    Production code

--- server/index.js
+++ server/index.js
@@ -33,9 +33,7 @@ app.use("/api/chat", chatRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
-  });
+
 }
 
 app.listen(PORT, () => {
